c0df6fabe36d7f76566bb85039bbf5c2a95c1b18
o0oDeMiaNo0o <demian.uruguay@gmail.com>
2017-06-07 17:40:20 -0300

    Firefox Addon
    Addon de Firefox que permite el ingreso de los usuarios a la mesa de ayuda

---
+++ FireFox Addon/background/main.js
@@ -0,0 +1,27 @@
+function notifyUser(user) {
+  browser.notifications.create({
+    "type": "basic",
+    "title": "Laboratorio de RIA",
+    "message": `Hola ${user.name}`
+  });
+   browser.tabs.create({
+     "url": "https://ria-demianpizzo.c9users.io/GoogleLogin.php"
+   });
+}
+
+function logError(error) {
+  console.error(`Error: ${error}`);
+}
+
+/**
+When the button's clicked:
+- get an access token using the identity API
+- use it to get the user's info
+- show a notification containing some of it
+*/
+browser.browserAction.onClicked.addListener(() => {
+  getAccessToken()
+    .then(getUserInfo)
+    .then(notifyUser)
+    .catch(logError);
+});
